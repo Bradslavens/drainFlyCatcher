6be079e34a06bf53311465ba9482f8ab984b8398
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-23 10:39:20 -0700

    random generate flys

---
+++ Assets/FlyGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyGenerator : MonoBehaviour
+{
+    public GameObject fly;
+    void Start()
+    {
+        for(int i = 0; i<10; i++)
+        { 
+            // Instantiate Jars randomly
+            Instantiate(fly, new Vector3(Random.Range(-10.0f, 10.0f),1,Random.Range(-10.0f,10.0f)), Quaternion.identity);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/JarController.cs
+++ Assets/JarController.cs
@@ -6,12 +6,6 @@ public class JarController : MonoBehaviour
 {
     public float jarSpeed = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Instantiate Jars randomly
-    }
-
     // Update is called once per frame
     void Update()
     {
