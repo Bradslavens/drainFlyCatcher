3bf5a800acb84cd3ea03811598b278e04c60b677
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-23 09:24:09 -0700

    first commit

---
+++ Assets/JarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JarController : MonoBehaviour
+{
+    public jarSpeed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate Jars randomly
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
