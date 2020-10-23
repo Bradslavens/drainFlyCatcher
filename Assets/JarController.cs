e5fce5ac35ecd110b9fdd817d396da5cb9e39969
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-23 15:25:20 -0700

    prevent player from falling off board

--- Assets/JarController.cs
+++ Assets/JarController.cs
@@ -12,27 +12,47 @@ public class JarController : MonoBehaviour
     private int score = 0;
 
     private void Start() {
-        scoreText.text  = "Score: 0";
+        scoreText.text  = "Score: 0/10";
     }
 
     // Update is called once per frame
     void Update()
     {
+            
         if(Input.GetKey(KeyCode.W)){
             this.transform.position += Vector3.forward * jarSpeed;
+
+            if(transform.position.z > 14){
+                transform.position = new Vector3(transform.position.x, transform.position.y, 14);
+
+                Debug.Log("transform z " + transform.position.z.ToString());
+            }
         }
         
         if(Input.GetKey(KeyCode.A)){
             this.transform.position += Vector3.left * jarSpeed;
+
+            if(transform.position.x < -14){
+                transform.position = new Vector3(-14, transform.position.y, transform.position.z);
+            }
         }
         
         if(Input.GetKey(KeyCode.S)){
             this.transform.position += Vector3.back * jarSpeed;
+
+            if(transform.position.z < -14){
+                transform.position = new Vector3(transform.position.x, transform.position.y, -14);
+            }
         }
         
         if(Input.GetKey(KeyCode.D)){
             this.transform.position += Vector3.right * jarSpeed;
+
+            if(transform.position.x > 14){
+                transform.position = new Vector3(14, transform.position.y, transform.position.z);
+            }
         }
+
     }
 
     private void OnCollisionEnter(Collision other) {
@@ -41,7 +61,8 @@ public class JarController : MonoBehaviour
 
             // add fly to UI
             score++;
-            scoreText.text = "Score: " + score;
+
+            scoreText.text = "Score: " + score + "/10";
         }
     }
 
